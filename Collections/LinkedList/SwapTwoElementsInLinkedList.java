cf40ea478bba5c11b4e750c1d3bd100bdc385e2a
Abdullah Mohammed Sabir <60984471+AbdullahBarkaati@users.noreply.github.com>
2024-01-15 15:03:26 +0530

    Create SwapTwoElementsInLinkedList.java

---
+++ Collections/LinkedList/SwapTwoElementsInLinkedList.java
@@ -0,0 +1,24 @@
+//Write a Java program that swaps two elements in a linked list.
+
+import java.util.*;
+
+public class SwapTwoElementsInLinkedList {
+
+    public static void main(String[] args) {
+
+        LinkedList <String> l_list = new LinkedList <String> ();
+        l_list.add("Red");
+        l_list.add("Green");
+        l_list.add("Black");
+        l_list.add("Pink");
+        l_list.add("orange");
+        System.out.println("The Original linked list: " + l_list);
+        int pos1, pos2;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two positions to swap.....");
+        pos1 = sc.nextInt();
+        pos2 = sc.nextInt();
+        Collections.swap(l_list, pos1, pos2);
+        System.out.println("The New linked list after swap: " + l_list);
+    }
+}
