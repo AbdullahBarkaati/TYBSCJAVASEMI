fbb891137c246ad772ae09c469f8aa13758efad3
Abdullah Mohammed Sabir <60984471+AbdullahBarkaati@users.noreply.github.com>
2024-01-15 15:15:37 +0530

    Create SortMapByUsingComparator.java

---
+++ Collections/HashMap/SortMapByUsingComparator.java
@@ -0,0 +1,34 @@
+//Write a program to sort HashMap by keys and display the details before sorting and after sorting.
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.Iterator;
+
+public class SortMapByUsingComparator
+{
+    public static void main(String args[])
+    {
+        HashMap<Integer, String> hm=new HashMap<>();
+        hm.put(22, "Javeriya");
+        hm.put(17, "Hadiya");
+        hm.put(15, "Afeefa");
+        hm.put(9, "Aaisha");
+
+        Iterator <Integer> it = hm.keySet().iterator();
+        System.out.println("Before Sorting");
+        while(it.hasNext())
+        {
+            int key=it.next();
+            System.out.println("Roll no:  "+key+"     name:   "+hm.get(key));
+        }
+        System.out.println("\n");
+
+        System.out.println("After Sorting");
+        TreeMap<Integer,String> tm=new  TreeMap<> (hm);
+        Iterator itr=tm.keySet().iterator();
+        while(itr.hasNext())
+        {
+            int key=(int)itr.next();
+            System.out.println("Roll no:  "+key+"     name:   "+hm.get(key));
+        }
+    }
+}
